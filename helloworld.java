22fb9c347d02cb1a1ee5feb58b3edcaaf8dccdd3
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2022-04-25 23:01:03 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+class helloworld
+    {
+        public static void main(String args[])
+        {
+            System.out.println("hello world");
+        }
+    }
+
