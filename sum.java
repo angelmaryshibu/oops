c04a3f5ad3fb0060d47ee78c9125a5875ddc12b6
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2022-04-25 23:09:38 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+class sum
+    {
+        public static void main(String args[])
+        {
+            Scanner s=new Scanner(System.in);
+            int a,b,sum=0;
+            System.out.println("enter 2 numbers");
+            a=s.nextInt();
+            b=s.nextInt();
+            sum=a+b;
+            System.out.println("the sum is:"+sum);
+        }
+    }
+
